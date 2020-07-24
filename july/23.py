ac0a57d0fb86dfa6c8c4118b98a4a83bb4ffce39
Shailesh Kumar <skbhagat40@gmail.com>
2020-07-24 08:08:56 +0530

    Update 23.py

--- july/23.py
+++ july/23.py
@@ -6,4 +6,20 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         c = Counter(nums)
         return filter(lambda x: c[x]==1, c)
+
+# without using extra space.
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        res = 0
+        for el in nums:
+            res = res ^ el
+        i = 0
+        while not(res & 1<<i):
+            i += 1
+        xor1, xor2 = 0, 0
+        for el in nums:
+            if el & (1<<i):
+                xor1 ^= el
+            else:
+                xor2 ^= el
+        return [xor1, xor2]
         
