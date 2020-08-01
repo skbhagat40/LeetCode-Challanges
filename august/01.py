3b93b4100dc79b62cbfd5c6967a241de8fa70816
Shailesh Kumar <skbhagat40@gmail.com>
2020-08-01 18:08:55 +0530

    Update 01.py

--- august/01.py
+++ august/01.py
@@ -24,3 +24,35 @@ class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         return word == word.lower() or word == word.upper() or word == word.title()
         
+"""
+Design a logger system that receive stream of messages along with its timestamps, each message should be printed if and only if it is not printed in the last 10 seconds.
+
+Given a message and a timestamp (in seconds granularity), return true if the message should be printed in the given timestamp, otherwise returns false.
+
+It is possible that several messages arrive roughly at the same time.
+"""
+class Logger:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.messages = {}
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        """
+        ts = self.messages.get(message, None)
+        if ts is None or timestamp >= ts + 10:
+            self.messages[message] = timestamp
+            return True
+        else:
+            return False
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
