798e1b2aacd83d79f098b3d353138812b043051a
Shailesh Kumar <skbhagat40@gmail.com>
2020-11-02 11:11:11 +0530

    Create 01.py

---
+++ november/01.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        s = ''
+        current = head
+        while current:
+            s += str(current.val)
+            current = current.next
+        return int(s, 2)
